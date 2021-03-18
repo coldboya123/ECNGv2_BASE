2c7597918da479db786af0c9dcc73878972b94f5
Milkyway <duchuy96itc@gmail.com>
2021-03-18 16:48:13 +0700

    18/3

--- app/src/main/java/com/example/ecngv2/View/Payment/PaymentSuccessActivity.java
+++ app/src/main/java/com/example/ecngv2/View/Payment/PaymentSuccessActivity.java
@@ -7,17 +7,27 @@ import android.os.Bundle;
 
 import com.example.ecngv2.R;
 import com.example.ecngv2.View.MainActivity.MainActivity;
+import com.example.ecngv2.View.OrderManager.OrderManagerActivity;
 
 public class PaymentSuccessActivity extends AppCompatActivity {
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_success);
+        new Thread(() -> {
+            try{
+                Thread.sleep(3000);
+            } catch (Exception e){
+
+            }finally {
+                intent = new Intent(getBaseContext(), OrderManagerActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }).start();
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-    }
 }
